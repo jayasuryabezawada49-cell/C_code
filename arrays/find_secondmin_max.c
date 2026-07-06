20097e67f5cc9191da4b4161c38a1145e66ea807
jayasuryabezawada49-cell <jayasuryabezawada49@gmail.com>
2026-07-06 10:46:11 +0530

    Update find_secondmin_max.c

--- arrays/find_secondmin_max.c
+++ arrays/find_secondmin_max.c
@@ -1,38 +1,29 @@
-#include <stdio.h>
-
+#include<stdio.h>
+#include<limits.h>
 int main()
 {
-   int size;
-   printf("enter number of elments=");
-   scanf("%d",&size);
-   int arr[size],min,max;
-   int second_max,second_min;
-   //taking elmnts
-   printf("enter the %d elments\n",size);
+    int arr[8]={45, 12, 78, 34, 89, 23, 67, 10};
    
-   for(int i=0;i<size;i++)
-   {
-       scanf("%d",&arr[i]);
-   }
-   
-   min=arr[0];
-   max=arr[0];
-   second_max=max; second_min=min;
-   for(int i=0;i<size;i++)
-   {
-       if(arr[i]>max)
-       {
-           second_max=max;
-           max=arr[i];
-       }
-       if(arr[i]<min)
-       {
-           second_min=min;
-           min=arr[i];
-       }
-   }
-   
-   //
-   printf("printing min=%d \nmax=%d\n",second_min,second_max);
-
+    int max=INT_MIN;
+    int min=INT_MAX;
+    int second_max,second_min;
+    
+    for(int i=0;i<8;i++)
+    {
+        
+    
+            if(arr[i]>max)
+            {
+                second_max=max;
+                max=arr[i];
+            }
+            else if(arr[i]<min)
+            {
+                second_min=min;
+                min=arr[i];
+            }
+        
+    }
+    printf("second_max=%d\n",second_max);
+    printf("second_min=%d\n",second_min);
 }
